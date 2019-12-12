7388e70d82f7f0f9260d5955eee7395a1bf1cd31
milla95 <>
2019-12-12 11:59:34 +0100

    add row and add column added defined without impl other needed methods

--- Source_code/Project-Spreadsheet/SpreadSheet/src/spreadsheet/client/Client.java
+++ Source_code/Project-Spreadsheet/SpreadSheet/src/spreadsheet/client/Client.java
@@ -32,6 +32,14 @@ public class Client
     {
         spreadsheet = new SpreadSheet(numRows, numCols);
     } 
+    public void addRow(int numOfRows)
+    {
+        spreadsheet.addRows(numOfRows);
+    }
+    public void addCol(int numOfCols)
+    {
+        spreadsheet.addRows(numOfCols);
+    }
     
     public static void createSpreadSheetOption(Client client)
     {
@@ -43,6 +51,46 @@ public class Client
         System.out.println("Spreadsheet created!");
     }
     
+    public static void addRowOption(Client client)
+    {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter number of rows to add: <int>");      
+        int x = scn.nextInt();
+        client.addRow(x);
+        System.out.println("Row added!");  
+    }
+    
+    
+    public static void addColOption(Client client)
+    {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter number of columns to add: <int>");      
+        int x = scn.nextInt();
+        client.addCol(x);
+        System.out.println("Col added!"); 
+    }
+    
+    public static void editCellOption(Client client)
+    {
+        // TODO  
+    }
+    
+    public static void getCellValueOption(Client client)
+    {
+        // TODO
+    }
+    
+    public static void exportSpreadSheet(Client client)
+    {
+        // TODO  
+    }
+    
+    public static void importSpreadSheet(Client client)
+    {
+        // TODO
+    } 
+    
+    
     public static void main(String[] args) 
     {
         Client client = new Client();
@@ -51,11 +99,19 @@ public class Client
         {
             System.out.println("Choose an option of below by entering the option number: ");
             System.out.println("1 Create an spreadsheet");
+            System.out.println("2 Add columns to spreadsheet");
+            System.out.println("3 Add rows to spreadsheet");
             int option = scn.nextInt();
             switch(option)
             {
                 case 1:
                     createSpreadSheetOption(client);
+                    break;
+                case 2:
+                    addColOption(client);
+                    break;
+                case 3:
+                    addRowOption(client);
                     break;
                 default:
                     System.out.println("Unrecognized option!");                   
